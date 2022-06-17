419c393ca787bc3055aec18922ff4363fa570570
carlton <carltonmbaj0@gmail.com>
2022-06-17 11:20:50 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function isPalindrome(word) {
+//the function is to take in a string as a parameter.
+function isPalindrome(string) {
   // Write your algorithm here
+
+  //first we find the length of the string
+  const lengthOfString = string.length;
+
+  //looping through half of the string (palindrome)
+  for (let i = 0; i < lengthOfString / 2; i++) {
+    //Now check if the last string and first are same
+    if (string[i] !== string[lengthOfString - 1 - i]) {
+      return "It is not a palindrome";
+    }
+  }
+  return "It's a palindrome";
 }
 
-/* 
-  Add your pseudocode here
-*/
+const string = prompt("Enter a string: ");
+
+// call the function
+const value = isPalindrome(string);
+
+console.log(value);
 
 /*
   Add written explanation of your solution here
